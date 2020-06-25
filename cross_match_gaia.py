a6b4f3481901a89e99435d6bc1d2ebcd8a0514e7
Sara Webb <swebb@swin.edu.au>
2020-06-25 21:25:04 +1000

    updated cross matches

--- cross_match_gaia.py
+++ cross_match_gaia.py
@@ -31,6 +31,10 @@ from astropy.coordinates import Angle
 import glob
 import csv
 
+field = 'Dusty10'
+year = '2018'
+month = '06'
+ran = '500'
 
 def getstars(filename):
     ra = [] ; dec = [] ; par = []; epar = []
@@ -40,9 +44,9 @@ def getstars(filename):
         if len(line)>0:
             line_split = line.split()
             if len(line_split)>=25:
-                ra.append(float(line_split[4]))
-                print(line_split[4])
-                dec.append(float(line_split[6]))
+                ra.append(float(line_split[0]))
+                #print(line_split[4])
+                dec.append(float(line_split[2]))
                 #gaiaid.append(int(line_split[2]))
                         #par.append(float(line_split[5]))
                         #epar.append(float(line_split[6]))
@@ -57,23 +61,25 @@ def getstars(filename):
     return ra,dec
 
 
-filename1 = '/home/swebb/oz100/NOAO_archive/archive_NOAO_data/scripts/create_lc/total_field_sources/fields/2016_07_14hr_full_field_sources.ascii'
-filename2 = '/home/swebb/oz100/NOAO_archive/archive_NOAO_data/scripts/GAIA_volume_searchs/100pc/14hrfield.100pc.sample'
+filename1 = '/home/swebb/oz100/NOAO_archive/archive_NOAO_data/scripts/create_lc/total_field_sources/fields/'+ year + '_'+month+'_'+ field + '_full_field_sources.ascii'
+filename2 = '/home/swebb/oz100/NOAO_archive/archive_NOAO_data/scripts/GAIA_volume_searchs/'+ran+'pc/'+field+'field.'+ran+'pc.sample'
 
 ra1, dec1 = np.loadtxt(filename1, skiprows=1, unpack = True)
-ra, dec = getstars(filename2)
-print(ra, dec)
-   
-
-'''
+ra2, dec2 = getstars(filename2)
+#print(ra)
+#ra1 = []
+#for i in ra:
+	#ra1.append(i/15)
 first_cat = np.empty((len(ra1), 2), dtype = np.float64)
-first_cat[:, 0] = ra1 
+first_cat[:, 0] = ra1
 first_cat[:, 1] = dec1
 
 new_cat = np.empty((int(len(ra2)), 2), dtype = np.float64)
 new_cat[:,0] = ra2
 new_cat[:,1] = dec2
 
+print(print(new_cat))
+print(print(first_cat))
 max_radius = 1/3600 #.5 arc second
 
 dist_between, ind_row = crossmatch_angular(first_cat, new_cat, max_radius)
@@ -149,11 +155,13 @@ match_true_table = Table ()
 match_true_table['RA'] = new_cat_RA_true
 match_true_table['DEC'] = new_cat_DEC_true
 
-
+print(match_true_table)
 match_false_table = Table()
 match_false_table['RA'] = new_cat_RA_false
 match_false_table['DEC'] = new_cat_DEC_false
 
-print('TRUE: ' + str(len(match_true_table)))
-print('FALSE: ' + str(len(match_false_table)))
-'''
+output = year + '_' + month+ '_'+field+'_'+ran+'_gaia_matched.ascii'
+
+match_true_table.write(output, format='ascii', overwrite=True)
+#print('TRUE: ' + str(len(match_true_table)))
+#print('FALSE: ' + str(len(match_false_table)))
